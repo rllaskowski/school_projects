72ed46841be39058b43a89c33b3d768edf09a84d
rllaskowski <robert.laskowski0@gmail.com>
2020-03-30 15:39:47 +0200

    comments/refactor

--- SO/DCL/dcl.asm
+++ SO/DCL/dcl.asm
@@ -18,8 +18,7 @@ permRevL        resb PERM_LEN
 permRevR        resb PERM_LEN
 permL           resb 8
 permR           resb 8
-permT           resb 8       
-key             resb 8
+permT           resb 8
 
 section .text
 
@@ -28,6 +27,10 @@ section .text
     call        _check_sign
 %endmacro
 
+; Makro wywołujące procedurę _check_param
+; argumenty:    %1 - adres w pamieci wskazujacy na parametr
+;               %2 - oczekiwana wartosc occur[x] dla znaku z kodem x przed wywolaniem procedury 
+;               %3 - oczekiwana długosc parametru
 %macro checkParam 3
     mov         rsi, %1
     mov         bl,  %2
@@ -36,52 +39,64 @@ section .text
 %endmacro
 
 ; Makro zdejmujace wartosc ze stosu i kopiujace te wartosc pod dany adres
-; argumenty:    1 - adres w pamieci do skopiowania wartosci ze stosu
+; argumenty:    %1 - adres w pamieci do skopiowania wartosci ze stosu
+; wyjscie:      rax - zdjeta ze stosu wartosc
 %macro popTo 1
-    pop         rax
+    pop         rax                     
     mov         %1, rax 
 %endmacro
 
 
 ; Makro wywolujace funkcję systemową sys_exit z odpowiednim sygnalem wyjscia
-; argumenty:    1 - sygnal wyjscia
+; argumenty:    %1 - sygnal wyjscia
 %macro  exit 1
-    mov         eax, SYS_EXIT
-    mov         edi, %1
-    syscall
+    mov         eax, SYS_EXIT           ; ustaw odpowiednia funkcje systemowa -sys_exit
+    mov         edi, %1                 ; ustaw sygnal wyjscia
+    syscall                         
 %endmacro
 
+; Makro modulujące liczbę nieujemną przez PERM_LEN
+; argumenty:    %1 - rejestr z liczbą do modulowania
+; wyjscie:      %1 - rejestr z przemodulowana liczba
 %macro mod 1
-    cmp         %1, PERM_LEN
-    jl          %%no_mod
-    sub         %1, PERM_LEN
+    cmp         %1, PERM_LEN            ; maksymalna dopuszczalna wartosc to PERM_LEN-1
+    jl          %%no_mod                ; jesli %1 jest w zakresie <0,PERM_LEN-1> nic nie rob
+    sub         %1, PERM_LEN            ; odejmij PERM_LEN jesli %1 jest za duze
 %%no_mod:
 %endmacro
 
+; Makro wykonujace permutacje Q
+; argumenty:    %1 - argument permutacji Q - wartosc w <0,PERM_LEN-1>
+; wejcie:       al - kod znaku w przediale <0,PERM_LEN-1>
+; wyjsci:       al - przepermutowany kod znaku w przedziale <0,PERM_LEN-1>
 %macro permQ    1
     add         al, %1 
     mod         al
 %endmacro
 
-%macro permQRev 1
-    add         al, PERM_LEN
-    sub         al, %1 
-    mod         al
+; Makro wykonujace permutacje Q^-1
+; argumenty:    %1 - argument permutacji Q^-1 - wartosc w <0,PERM_LEN-1>
+; wejcie:       al - kod znaku w przediale <0,PERM_LEN-1>
+; wyjscie:       al - przepermutowany kod znaku w przedziale <0,PERM_LEN-1>
+%macro permQRev 1          
+    add         al, PERM_LEN            ; dodanie PERM_LEN zapewnia, że al bedzie liczba calkowita
+    sub         al, %1                  ; wykonaj permutacje Q^-1 z odpowiednim argumentem
+    mod         al          
 %endmacro
 
-; Wywoluje procedure _create_rev obliczającą permutację odwrotną
-; argumenty:    1 - adres permutacji
-;               2 - adres wynikowej permutacji odwrotnej
+; Makro wwolujejace procedure _create_rev obliczającą permutację odwrotną
+; argumenty:    %1 - adres permutacji
+;               %2 - adres wynikowej permutacji odwrotnej
 %macro createRev 2
-    mov         rax, %1
-    mov         rbx, %2
+    mov         rax, %1                 ; _create_rev przyjmuje w rax adres tablicy permutacji 
+    mov         rbx, %2                 ; _create_rev przyjmuje w rbx adres wynikowej permutacji odwrotnej
     call        _create_rev
 %endmacro
 
 ; Makro zamieniające znak korzystajac z odpowiedniej permutacji
-; argumenty:    1: adres permutacji
+; argumenty:    1 - adres permutacji
 ; wejscie:      kod znaku w rax
-; wyjcie:       przepermutowany kod znaku w rax
+; wyjcie:       przepermutowany kod znaku w al
 %macro perm 1
     mov         rsi, %1                 ; kopiuj do rsi adres permutacji
     mov         al, [rsi+rax]           ; wykonaj odpowiednia permutacje znaku
@@ -107,21 +122,23 @@ _create_rev_loop:
 
 ; Sprawdza poprawnosc znaku i zmniejsza jego kod o '1'. Konczy program jesli znak jest niepoprawny
 ; wejscie:      al - kod znaku
-; wyjscie       al - zmniejszony o '1' kod znaku 
+; wyjscie       al - zmniejszony o '1' kod znaku - sprowadzony do przedzialu <0,PERM_LEN-1>
 _check_sign: 
     cmp         al, '1'+PERM_LEN-1      ; porownaj kod znaku z najwieszym mozliwym
     jg          _error                  ; jesli jest wiekszy niz 'Z' koncze program
     cmp         al, '1'                 ; porownuje znak z najmniejszym mozliwym
     jl          _error                  ; jesli jest mniejszy niz '1' koncze program
-    sub         al, '1'                 ; wszystkie kody sprowadzam do przedzialu <0, 41>
+    sub         al, '1'                 ; kod sprowadz do przedzialu <0, PERM_LEM-1>
     ret
 
 ; Procedura sprawdzająca poprawność permutacji. Konczy program z sygnalem bledu
 ; jesli rozpatrywana permutacja jest niepoprawna
 ; wejscie:      rsi - adres tablicy z permutacja
-;               bl - oczekiwana ilosc wystapien znaku przed rozpoczeciem procedury
+;               bl - oczekiwana wartosc occur[x] dla znaku x przed wywolaniem procedury
 ;               bh - oczekiwana dlugosc parametru
-; wyjscie:      kazdy kod znaku w tablicy z permutacja zostaje zmniejszony o kod '1'
+; wyjscie:      rax - 0
+;               rcx - dlugosc parametru
+;               kazdy kod znaku w tablicy z permutacja zostaje zmniejszony o kod '1'         
 _check_param:
     xor         rax, rax                ; wartosc calego rax bedzie rowna kodowi znaku
     xor         rcx, rcx                ; zeruj licznik znakow
@@ -149,6 +166,9 @@ _check_param_end:
 
 ; Procedura sprawdzajaca poprawnosc permutacji T
 ; konczy program z sygnalem bledu jesli permutacja jest niepoprawna
+; wyjscie:      rax - adres permutacji T
+;               rcx - 0
+;               bl, dl - smieci zalezne od przejscia algorytmu
 _check_t:
     mov         rcx, PERM_LEN-1         ; ustaw licznik petli do chodzenia po permutacji
     xor         rbx, rbx                ; wartosc rbx bedzie rowna kodowi znaku
@@ -168,10 +188,10 @@ _check_t_loop:
 ; wejscie:      rax - kod znaku w przedziale <0,PERM_LEN-1>
 ;               bh - pozycja bebenka l w przedziale <0,PERM_LEN-1>
 ;               bl - pozycja bebenka r w przedziale <0,PERM_LEN-1>
-; wyjscie       rax - przepermutowany kod znaku 
+; wyjscie       al - przepermutowany kod znaku w przedziale <0,PERM_LEN-1>
 _parse_sign:
     inc         bl                      ; zwieksz pozycje bębenka r
-; sprawdzamy czy bebenek l jest na ktorejs z pozycji obrotowych
+; sprawdz czy bebenek r jest na ktorejs z pozycji obrotowych
     cmp         bl, 'L'-'1'             ; czy r jest na na L?
     je          _turn                   ; jesli tak przekrec l
     cmp         bl, 'R'-'1'             ; czy r jest na R?
@@ -199,8 +219,8 @@ _no_turn:
     perm        permRevR                ; wykanaj perm -R
     permQRev    bl                      ; wykonaj perm -Q_r
 
-    add         al, '1'                 ; zmien spowrotem na znak do wypisania
     ret
+; Glowna procedura programu
 _start:
     pop          rax                    ; zdejmij ze stosu liczbe argumentow
     cmp          rax, 5                 ; ilość argumentów na wejsciu powinna wynoscic 5
@@ -221,9 +241,9 @@ _start:
 
     call        _check_t                ; sprawdz poprawnosc permutacji T
 
-    pop         r10                     ; zdejmij do r10 adres klucza
-    checkParam  r10, 0, 2               ; sprawdz poprawnosc klucza
-    mov         bx, [r10]               ; wstaw do bx klucz - w bh poczatkowa pozycja l - w bl poczatkowa pozycja r
+    pop         r11                     ; zdejmij do r10 adres klucza
+    checkParam  r11, 0, 2               ; sprawdz poprawnosc klucza
+    mov         bx, [r11]               ; wstaw do bx klucz - w bh poczatkowa pozycja l - w bl poczatkowa pozycja r
 _input_loop:
     mov         rax, SYS_READ           ; ustaw do wywołania funkcję sys_read
     xor         edi, edi                ; korzystaj ze standardowego wejscia - STDIN = 0
@@ -231,18 +251,20 @@ _input_loop:
     mov         rsi, buffer             ; ustaw adres czytania na adres bufora
     syscall                             ; wywołaj funkcję systemową
                       
-    test        eax, eax                ; jesli wczytano 0 ilosc znakow
+    test        eax, eax                ; jesli wczytano 0 znakow
     jz          _input_end              ; skoncz wczytywanie
 
-    mov         edx, eax                ; w rdx trzymamy ilosc znakow do wypisania = ilosci wczytanch znakow
+    mov         edx, eax                ; w dx trzymamy ilosc znakow do wypisania = ilosci wczytanch znakow
     xor         rcx, rcx                ; inicjalizuj licznik petli
 _check_loop:
     mov         al, [buffer+rcx]        ; kopiuj aktualny znak z bufora
     call        _check_sign             ; czy rozpatrywany znak jest poprawny? 
+
     call        _parse_sign             ; koduj znak
-    mov         [buffer+rcx], al        ; wstaw zakodowany znak do bufora
+    add         al, '1'                 ; zmien spowrotem na kod znaku do wypisania
+    mov         [buffer+rcx], al        ; wstaw przepermutowany znak do bufora   
     inc         cx                      ; zwieksz licznik petli
-    cmp         cx, dx                  ; czy zostaly przejrzane wszystkie znaki? na gorze stosu jest liczba wczytanych znakow
+    cmp         cx, dx                  ; czy zostaly przejrzane wszystkie znaki? 
     jne          _check_loop            ; jesli nie kontynuuj parsowanie bufora
  
     mov         eax, SYS_WRITE          ; ustaw do wywołania funkcję sys_write
